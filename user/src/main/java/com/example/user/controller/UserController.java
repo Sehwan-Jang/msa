3e7ce6b471ace682554ea803eb9f3f411156f75e
sehwan.jang <whilter08@direa.co.kr>
2023-08-08 10:47:31 +0900

    chore: Configure user service

---
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private final Greeting greeting;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return greeting.getMessage();
+    }
+}
